f7d3e1fb9c0ecebb23a233072e1618067eeca47e
anshulsharma48 <70880133+anshulsharma48@users.noreply.github.com>
2026-02-12 23:59:00 +0530

    Add files via upload

---
+++ using.js
@@ -0,0 +1,7 @@
+const accountName = "Anshul";
+var accfullname = "Anshul Sharma";
+let accountlasty = "ANshul hu mai";
+
+ accountcity="Delhi"
+let accountState;
+console.table([accfullname,accountName,accountlasty,accountcity,accountState])
