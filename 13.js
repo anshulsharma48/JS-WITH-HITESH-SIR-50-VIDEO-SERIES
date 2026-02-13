75d68f092c58c7c54d811eea041280fb3d16e8e5
anshulsharma48 <70880133+anshulsharma48@users.noreply.github.com>
2026-02-13 17:24:33 +0000

    data

---
+++ 13.js
@@ -0,0 +1,26 @@
+// "use strict";
+// // alert(hello) we are using node js,  not browser
+// // 
+// console.log(typeof "naame" );
+
+
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInInt=Number(score)
+// console.log(typeof valueInInt);
+// console.log(valueInInt)
+
+let score=0;
+
+// let valueOfcore=Boolean(score)
+// console.log(typeof valueOfcore);
+// console.log(valueOfcore);
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
