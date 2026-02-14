efa6acbf6aba3bf2084bfeda43324587c66e3906
anshulsharma48 <70880133+anshulsharma48@users.noreply.github.com>
2026-02-14 23:38:56 +0530

    Add files via upload

---
+++ 14feb.js
@@ -0,0 +1,18 @@
+// const name='anshul '
+// const fullname="sharma"
+// const fname=(name+fullname)
+// greet = ("Goodmorning " + fname)
+// // console.log(name + fullname);
+
+
+
+// console.log((`hello this is me ${greet} and you can see that ${fname}`));
+
+
+// declaring string through OBJECT
+ const name= new String("ANSHUL SHARMA ")
+
+//  console.log(name);
+//  console.log(name.toLowerCase());
+
+ console.log(name.includes("SHARMA"))
