77145774b1ac674a41aa267a6e75b57a5e27826e
anshulsharma48 <70880133+anshulsharma48@users.noreply.github.com>
2026-02-15 21:55:43 +0530

    Add files via upload

---
+++ dates.js
@@ -0,0 +1,19 @@
+// let myDate=new Date()
+// // // console.log(myDate.toString())
+// // console.log(myDate.toLocaleString());
+
+
+// //CREATING OWN DATE 
+
+// let myCreatedDate=new Date()
+// // console.log(myCreatedDate.toDateString());
+
+// // let myCreatedDate2 = new Date("2025-02-12") 
+// // console.log(myCreatedDate2.toDateString());
+
+
+// let MyTimeStamp=Date()
+
+
+let newDate=new Date()
+console.log(newDate.getDate()+1)
