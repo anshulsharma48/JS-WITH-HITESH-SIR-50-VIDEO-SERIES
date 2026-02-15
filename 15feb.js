8963e98f304e6c64fc62ccbd9b756047d0a150f0
anshulsharma48 <70880133+anshulsharma48@users.noreply.github.com>
2026-02-15 21:05:10 +0530

    Add files via upload

---
+++ 15feb.js
@@ -0,0 +1,18 @@
+// const hello=12345;
+
+// const helloo = new Number(1200);
+// console.log(helloo);
+
+// const othernumber=helloo
+
+
+// console.log(othernumber.toString().);
+
+// console.log(Math.max(12,23,3,3,333,4442));
+// console.log(Math.sqrt(25))
+
+console.log((Math.random()*10)+1)
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min )
